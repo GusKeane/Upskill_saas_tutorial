6ae599587295db61b9eb1e7826c6ce8a6abb3fd4
Angus Keane <a.keane92@gmail.com>
2017-02-03 03:13:58 +0000

    Corrected nav link route to contact page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
     root to: 'pages#home' 
     get 'about', to: 'pages#about'
-    get 'contact/new', to: 'contacts#new'
-    # resources :contacts 
+    resources :contacts 
 end
